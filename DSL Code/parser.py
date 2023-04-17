c4c49a182b5194e13d24a4d96c58d8a5ae26e9d8
Cas Ruedy <89892181+Cruedy@users.noreply.github.com>
2023-04-17 21:30:51 +0000

    shape

--- DSL Code/parser.py
+++ DSL Code/parser.py
@@ -1,3 +1,5 @@
+# trim off the end of the string when it has spaces
+# skip lines that have //
 def parser(file):
     f = open(file, "r")
     parse_lines = []
@@ -7,16 +9,9 @@ def parser(file):
             s = s.replace('\n', '')
         if s[:2] != "//" and s != "":
             parse_lines.append(s)
-    if parse_lines[0] == "SHAPE":
-        shape_parser(parse_lines[1:])
-    elif parse_lines[0] == "EFFECT":
-        effect_parser(parse_lines)
-
-def shape_parser(lines):
-    # need to check if the first line selects the composition
-    print(lines)
-    first_line = lines[0]
-    comp_parts = first_line.split(' = ')
+    type = parse_lines[0]
+    comp = parse_lines[1]
+    comp_parts = comp.split(' = ')
     comp_name = comp_parts[0]
     comp_type = comp_parts[1]
     if comp_type == 'current_comp()':
@@ -29,26 +24,24 @@ def shape_parser(lines):
     first_line = comp_name + " = " + comp_type
     script = open("shape.jsx","w")
     script.write(first_line)
+    if type == "SHAPE":
+        shape_parser(parse_lines[2:])
+    elif type == "EFFECT":
+        effect_parser(parse_lines[2:])
+
+def shape_parser(lines):
+    name = ""
+    for l in lines:
+        if '{' in l and l[0] != '{':
+            name = l[:l.index('{')]
+            lines.remove(l)
+            break
+    end = lines.index("}")
+    attributes = lines[:end]
+    name = name.replace("\"", "")
     
 
 def effect_parser(lines):
     print(lines)
-    first_line = lines[0]
-    comp_parts = first_line.split(' = ')
-    comp_name = comp_parts[0]
-    comp_type = comp_parts[1]
-    if comp_type == 'current_comp()':
-        comp_type = 'app.project.activeItem'
-    elif 'different_comp' in comp_type:
-        index = comp_type[len(comp_type)-2:-1]
-        comp_type = 'app.project.item(' + str(index) + ')'
-    else:
-        print("Error: Composition must be created first")
-    first_line = comp_name + " = " + comp_type
     
 parser("shape.txt")
-
-
-#
-
-
