93a62f5e57edab5429d4860dff53f58632f43c0f
Cas Ruedy <89892181+Cruedy@users.noreply.github.com>
2023-04-28 06:47:46 +0000

    created

--- DSL Code/effect.jsx
+++ DSL Code/effect.jsx
@@ -1,4 +1,4 @@
 var comp = app.project.activeItem;
 var layer = comp.selectedLayers[0];
-var layer = layer.property("ADBE Effect Parade").addProperty("ADBE Gaussian Blur 2");
-effect.property("ADBE Gaussian Blur 2-0001").setValue(90);
+var effect = layer.property("ADBE Effect Parade").addProperty("ADBE Gaussian Blur 2");
+effect.property("ADBE Gaussian Blur 2-0001").setValue(20);
