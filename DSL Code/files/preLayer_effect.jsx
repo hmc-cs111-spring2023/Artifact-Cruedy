ededacc43230183db6a4d260df8026cee0df55c4
Cas Ruedy <89892181+Cruedy@users.noreply.github.com>
2023-04-28 01:05:28 -0700

    Delete preLayer_effect.jsx

--- DSL Code/files/preLayer_effect.jsx
+++
@@ -1,4 +0,0 @@
-var comp = app.project.activeItem;
-var layer = comp.selectedLayers[0];
-var effect = layer.property("ADBE Effect Parade").addProperty("ADBE Gaussian Blur 2");
-effect.property("ADBE Gaussian Blur 2-0001").setValue(100);
